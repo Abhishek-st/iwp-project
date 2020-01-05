c3b0da9b22c8cc30418063dc4ef938777f15268b
Abhishek-st <ankit2632000@gmail.com>
2020-01-05 22:04:17 +0530

    checking

--- food/food/settings.py
+++ food/food/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'web',
 ]
 
 MIDDLEWARE = [
@@ -60,7 +61,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
+                'django.Charield.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
--- food/web/models.py
+++ food/web/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    cname = models.CharField(max_length=30)
+    cuname = models.CharField(primary_key=True, max_length=10)
+    dob = models.DateField()
